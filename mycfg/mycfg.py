c1574a2e9a5bfd20f123abcd0f6247ec187b4de6
Ernest Ng <eyn5@cornell.edu>
2025-01-27 17:08:00 -0500

    mycfg.py example from lesson 2

---
+++ mycfg/mycfg.py
@@ -0,0 +1,85 @@
+import json 
+import sys 
+from collections import OrderedDict
+
+TERMINATORS = ('jmp', 'br', 'ret')
+
+def form_blocks(body):
+    """Forms a basic block containing the instructions in `body`
+    """
+
+    cur_block = []
+
+    for instr in body:
+        if 'op' in instr:   # an actual instruction
+            cur_block.append(instr)
+
+            # Check for terminator (yield the current block to the client)
+            if instr['op'] in TERMINATORS:
+               yield cur_block 
+               cur_block = [] 
+               
+        else:   # A label 
+            yield cur_block 
+
+            # Put the label in the next basic block
+            cur_block = [instr]
+    
+    yield cur_block
+
+def block_map(blocks):
+    """Creates labels for a list of blocks 
+    (generating fresh labels when necessary)
+    """
+    out = OrderedDict()
+
+    for block in blocks:
+        # A label already exists (first instruction in the block)
+        if 'label' in block[0]:
+            name = block[0]['label']
+            block = block[1:]
+        else:
+            name = 'b{}'.format(len(out))
+
+        out[name] = block  
+
+    return out 
+
+def get_cfg(name2block):
+    """Given a name-to-block map, produces a map from block names to 
+    successor block names
+    """
+    out = dict()
+    for i, (name, block) in enumerate(name2block.items()):
+        last = block[-1]
+        if last['op'] in ('jmp', 'br'):
+            succ = last['labels']
+        elif last['op'] == 'ret':
+            succ = []
+        else:
+            # We've reached the last instruction in the block
+            if i == len(name2block) - 1:
+                succ = []
+            else:
+                # We fall through to the next block in `name2block`
+                succ = list(name2block.keys())[i + 1]
+
+        out[name] = succ
+    return out 
+ 
+
+# Constructs a CFG for a Bril program
+def mycfg():
+    prog = json.load(sys.stdin)
+    for func in prog['functions']:
+        # Get labelled basic blocks
+        name2block = block_map(form_blocks(func['instrs']))
+        for name, block in name2block.items():
+            print(name)
+            print('  ', block)
+
+        cfg = get_cfg(name2block)
+        print(cfg)
+
+if __name__ == '__main__':
+    mycfg()    
